f1fd69388a9f8e91e9cf918d6bd8e205764df77b
pgodino <pedrogod9@gmail.com>
2018-06-10 21:08:51 +0200

    añadidos algunos botones

--- src/com/iesvirgendelcarmen/proyecto/controlador/Controlador.java
+++ src/com/iesvirgendelcarmen/proyecto/controlador/Controlador.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.event.TableModelEvent;
@@ -40,6 +41,7 @@ public class Controlador implements ActionListener {
 
 	private void registrarComponentes() {
 		vista.getMntmCargarDatos().addActionListener(this);
+		vista.getBtnAnadirDatos().addActionListener(this);
 	}
 
 	@Override
@@ -54,49 +56,79 @@ public class Controlador implements ActionListener {
 				lanzarEleccionFichero();
 
 		}
+
+		//agruparemos jbutton
+		if (e.getSource().getClass() == JButton.class) {
+			JButton jButton = (JButton) e.getSource();
+			String textoBoton = jButton.getText();
+			System.out.println(textoBoton);
+			switch (textoBoton) {
+			case "Anadir datos":
+				System.out.println("pulsado " + textoBoton);
+				annadirDatos();
+				break;
+
+			default:
+				break;
+			}
+
+		}
+
+	}
+
+
+
+
+
+	private void annadirDatos() {
+
+		//	int resultado = JOptionPane.showConfirmDialog(null, textFields, "Añadir datos", JOptionPane.OK_CANCEL_OPTION);
+		CiudadDTO ciudad = new CiudadDTO(Integer.parseInt(vista.getTextFieldID().getText()), vista.getTextFieldCiudad().getText(), vista.getTextFieldPais().getText(), Double.parseDouble(vista.getTextFieldLatitud().getText()), Double.parseDouble(vista.getTextFieldLongitud().getText()));
+		manipularCiudades.insertarCiudad(ciudad);
+		
 	}
 
 	private boolean lanzarEleccionFichero() {
-		
-			if(listaCiudades.size() == 0) {
-				JFileChooser jFileChooser = new JFileChooser("./datos");
-				int resultado = jFileChooser.showOpenDialog(vista.getFrame());
-				if (resultado == jFileChooser.APPROVE_OPTION) {
+
+		if(listaCiudades.size() == 0) {
+			JFileChooser jFileChooser = new JFileChooser("./datos");
+			int resultado = jFileChooser.showOpenDialog(vista.getFrame());
+			if (resultado == jFileChooser.APPROVE_OPTION) {
 				path = jFileChooser.getSelectedFile().getPath();
 				listaCiudades = leerCSV.obtenerListaCiudadesCSV(path);
-		
+
 			}
-			}
-			vista.getMntmCargarDatos().setEnabled(false);
-			datos = new Object[listaCiudades.size()][5];
-			
-			for (int i = 0; i <listaCiudades.size(); i++) {
-				datos[i][0] = listaCiudades.get(i).getIdCiudad();
-				datos[i][1] = listaCiudades.get(i).getNombreCiudad();
-				datos[i][2] = listaCiudades.get(i).getNombrePais();
-				datos[i][3] = listaCiudades.get(i).getLatitud();
-				datos[i][4] = listaCiudades.get(i).getLongitud();
-			}
-			
-			mtTable = new ModeloTabla(datos, cabecera);
-			table = new JTable(mtTable);
-			scrollPane = new JScrollPane(table);
-			vista.getTabbedPane().addTab("Tabla", null, scrollPane, null);
-			
+		}
+		vista.getMntmCargarDatos().setEnabled(false);
+		datos = new Object[listaCiudades.size()][5];
+
+		for (int i = 0; i <listaCiudades.size(); i++) {
+			datos[i][0] = listaCiudades.get(i).getIdCiudad();
+			datos[i][1] = listaCiudades.get(i).getNombreCiudad();
+			datos[i][2] = listaCiudades.get(i).getNombrePais();
+			datos[i][3] = listaCiudades.get(i).getLatitud();
+			datos[i][4] = listaCiudades.get(i).getLongitud();
+		}
+
+		mtTable = new ModeloTabla(datos, cabecera);
+		table = new JTable(mtTable);
+		scrollPane = new JScrollPane(table);
+		vista.getTabbedPane().addTab("Tabla", null, scrollPane, null);
+
 		//	vista.getScrollPane().add(new JButton("hola"));
-			table.setFillsViewportHeight(true);
-			
-			if(manipularCiudades.listarCiudades().size()<=0) {
-				manipularCiudades.crearBaseDeDatos();
-				manipularCiudades.insertarListaCiudades(listaCiudades);
-				manipularCiudades.completarArrays(listaCiudades);
-			}
+		table.setFillsViewportHeight(true);
+
+		if(manipularCiudades.listarCiudades().size()<=0) {
+			manipularCiudades.crearBaseDeDatos();
+			manipularCiudades.insertarListaCiudades(listaCiudades);
+			manipularCiudades.completarArrays(listaCiudades);
+		}
 
 		/*	for (CiudadDTO ciudadDTO : listaCiudades) {
-				*
-			}*/
-			//System.out.println("TamaÃ±o lista " + listaLenguajes.size());
-			//System.out.println("TamaÃ±o conjunto " + conjutoLenguajes.size());
+		 *
+		}*/
+		//System.out.println("Tamaño lista " + listaLenguajes.size());
+		//System.out.println("Tamaño conjunto " + conjutoLenguajes.size());
 
 
 
@@ -107,12 +139,12 @@ public class Controlador implements ActionListener {
 		//	JScrollPane scrollPane = new JScrollPane(vista.getTable(),JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
 
-			//	pintarDatosTabla();
-			//ModeloTabla mtTabla = new ModeloTabla(manipularCiudades);
-			//JTable jtable = new JTable(mtTabla);
-			//jtable.getModel().addTableModelListener(this);
+		//	pintarDatosTabla();
+		//ModeloTabla mtTabla = new ModeloTabla(manipularCiudades);
+		//JTable jtable = new JTable(mtTabla);
+		//jtable.getModel().addTableModelListener(this);
 
-		
+
 		return true;
 
 
--- src/com/iesvirgendelcarmen/proyecto/vista/Vista.java
+++ src/com/iesvirgendelcarmen/proyecto/vista/Vista.java
@@ -2,6 +2,7 @@ package com.iesvirgendelcarmen.proyecto.vista;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import java.awt.BorderLayout;
@@ -10,6 +11,11 @@ import javax.swing.JMenu;
 import javax.swing.JTabbedPane;
 import javax.swing.JScrollPane;
 import javax.swing.JMenuItem;
+import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Vista {
 
@@ -18,7 +24,27 @@ public class Vista {
 	private JMenuItem mntmCargarDatos;
 	private JScrollPane scrollPane;
 	private JTabbedPane tabbedPane;
-
+	private JMenu mnDatos;
+	private JMenuBar menuBar;
+	private JButton btnAnadirDatos;
+	private JButton btnBorrarDatos;
+	private JButton btnActualizarDatos;
+	private JSplitPane splitPane;
+	private JPanel panelIzquierdo;
+	private JPanel panelDerecho;
+	private JLabel lblId;
+	private JTextField textFieldID;
+	private JLabel lblCiudad;
+	private JTextField textFieldCiudad;
+	private JLabel lblPais;
+	private JTextField textFieldPais;
+	private JLabel lblLatitud;
+	private JTextField textFieldLatitud;
+	private JLabel lblLongitud;
+	private JTextField textFieldLongitud;
+	private JPanel panelBotones;
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -51,28 +77,81 @@ public class Vista {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 500, 500);
+		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 		
-		JMenuBar menuBar = new JMenuBar();
+		menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
-		JMenu mnDatos = new JMenu("Datos");
+		mnDatos = new JMenu("Datos");
 		menuBar.add(mnDatos);
 		
 		mntmCargarDatos = new JMenuItem("Cargar datos");
 		mnDatos.add(mntmCargarDatos);
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		frame.getContentPane().add(tabbedPane, BorderLayout.NORTH);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		splitPane = new JSplitPane();
+		tabbedPane.addTab("Filtros", null, splitPane, null);
+		
+		panelIzquierdo = new JPanel();
+		splitPane.setLeftComponent(panelIzquierdo);
+		
+		panelDerecho = new JPanel();
+		splitPane.setRightComponent(panelDerecho);
+		panelDerecho.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		lblId = new JLabel("ID");
+		panelDerecho.add(lblId);
+		
+		textFieldID = new JTextField();
+		panelDerecho.add(textFieldID);
+		textFieldID.setColumns(10);
+		
+		lblCiudad = new JLabel("Ciudad");
+		panelDerecho.add(lblCiudad);
+		
+		textFieldCiudad = new JTextField();
+		panelDerecho.add(textFieldCiudad);
+		textFieldCiudad.setColumns(10);
+		
+		lblPais = new JLabel("Pais");
+		panelDerecho.add(lblPais);
+		
+		textFieldPais = new JTextField();
+		panelDerecho.add(textFieldPais);
+		textFieldPais.setColumns(10);
+		
+		lblLatitud = new JLabel("Latitud");
+		panelDerecho.add(lblLatitud);
+		
+		textFieldLatitud = new JTextField();
+		panelDerecho.add(textFieldLatitud);
+		textFieldLatitud.setColumns(10);
+		
+		lblLongitud = new JLabel("Longitud");
+		panelDerecho.add(lblLongitud);
+		
+		textFieldLongitud = new JTextField();
+		panelDerecho.add(textFieldLongitud);
+		textFieldLongitud.setColumns(10);
+		
+		panelBotones = new JPanel();
+		panelDerecho.add(panelBotones);
 	//	table = new JTable();
 	/*	scrollPane = new JScrollPane();
 		tabbedPane.addTab("Tabla", null, scrollPane, null);*/
-		
-		
+		btnAnadirDatos = new JButton("Anadir datos");
+		panelBotones.add(btnAnadirDatos);
+		btnBorrarDatos = new JButton("Borrar datos");
+		panelBotones.add(btnBorrarDatos);
+		btnActualizarDatos = new JButton("Actualizar datos");
+		panelBotones.add(btnActualizarDatos);
 		
 	}
 
@@ -95,5 +174,81 @@ public class Vista {
 	public void setTabbedPane(JTabbedPane tabbedPane) {
 		this.tabbedPane = tabbedPane;
 	}
+
+	public JMenu getMnDatos() {
+		return mnDatos;
+	}
+
+	public JMenuBar getMenuBar() {
+		return menuBar;
+	}
+
+	public JButton getBtnAnadirDatos() {
+		return btnAnadirDatos;
+	}
+
+	public JButton getBtnBorrarDatos() {
+		return btnBorrarDatos;
+	}
+
+	public JButton getBtnActualizarDatos() {
+		return btnActualizarDatos;
+	}
+
+	public JSplitPane getSplitPane() {
+		return splitPane;
+	}
+
+	public JPanel getPanelIzquierdo() {
+		return panelIzquierdo;
+	}
+
+	public JPanel getPanelDerecho() {
+		return panelDerecho;
+	}
+
+	public JLabel getLblId() {
+		return lblId;
+	}
+
+	public JTextField getTextFieldID() {
+		return textFieldID;
+	}
+
+	public JLabel getLblCiudad() {
+		return lblCiudad;
+	}
+
+	public JTextField getTextFieldCiudad() {
+		return textFieldCiudad;
+	}
+
+	public JLabel getLblPais() {
+		return lblPais;
+	}
+
+
+	public JLabel getLblLatitud() {
+		return lblLatitud;
+	}
+
+
+	public JLabel getLblLongitud() {
+		return lblLongitud;
+	}
+
+
+	public JTextField getTextFieldPais() {
+		return textFieldPais;
+	}
+
+	public JTextField getTextFieldLatitud() {
+		return textFieldLatitud;
+	}
+
+	public JTextField getTextFieldLongitud() {
+		return textFieldLongitud;
+	}
+	
 	
 }
